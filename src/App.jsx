39c8bf7a6f4aedf2bbd66365458f4085fef659ed
top1c <afoninartofficial@gmail.com>
2023-12-02 13:35:23 +0300

    Small fix with data update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import BirthdaysList from "./Components/BirthdaysList";
 import UpcomingBirthday from "./Components/UpcomingBirthday";
-// import { birthdays } from "./test_data";
-import { birthdays } from "./my_data";
+import { birthdays } from "./test_data";
+// import { birthdays } from "./my_data";
 import {
   convertStringMonthToNumber,
   getTargetYear,
@@ -42,6 +42,7 @@ function App() {
         };
       });
     });
+    console.log(1);
   }, []);
 
   useEffect(() => {
